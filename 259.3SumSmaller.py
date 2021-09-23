4df853d69d45a3d4a0fab9c54fe28052175238bb
HuZiqing <nicolas.huziqing@gmail.com>
2021-09-22 21:18:21 -0400

    .

--- 259.3SumSmaller.py
+++ 259.3SumSmaller.py
@@ -1,6 +1,5 @@
 class Solution:
-    def threeSumSmaller(self, nums: List[int], target: int) -> int:
-        
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:  
         """"
         Brute force require O(n^3) complexity, choose 3 from n
         Since we are quired to find less than, if we sort the nums, 
@@ -18,4 +17,28 @@ class Solution:
                 temp = target - nums[i] - nums[j]
                 ind = bisect.bisect_left(nums, temp, j+1) - 1
                 result += ind - j
+        return result
+    
+
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        "Two pointer, like the solution in 3 sum"
+        "can be extended to k sum smaller"
+        def twosumsmaller(nums, start, target):
+            lo, hi = start, len(nums) - 1
+            result = start
+            while lo < hi:
+                T = nums[lo] + nums[hi]
+                if T >= target:
+                    hi -= 1
+                else:
+                    result += hi - lo
+                    lo += 1
+            return result
+
+        nums.sort()
+        result = 0
+        for i in range(len(nums)):
+            # if i and nums[i] == nums[i - 1]: continue#  we dont need this line, because we are asked to find the number of index, not the distinct tuple.
+            result += twosumsmaller(nums, i+1, target - nums[i])
         return result
