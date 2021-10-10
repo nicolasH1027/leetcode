ac24179ed717dd1a83c113f943b91ab7c395f374
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-10 14:47:06 -0400

    .

---
+++ 569.MedianEmployeeSalary.sql
@@ -0,0 +1,17 @@
+-- # Write your MySQL query statement below
+-- abs(number of greater or equal to - number smaller or equal to ) <= 1 
+
+
+SELECT 
+    Id, Company, Salary
+FROM (
+    SELECT 
+        *, 
+        ROW_NUMBER() OVER(PARTITION BY COMPANY ORDER BY Salary ASC, Id ASC) AS RN_ASC,
+        ROW_NUMBER() OVER(PARTITION BY COMPANY ORDER BY Salary DESC, Id DESC) AS RN_DESC
+    FROM
+        Employee) AS temp
+WHERE 
+    RN_ASC BETWEEN RN_DESC - 1 AND RN_DESC + 1
+ORDER BY 
+    Company, Salary;
