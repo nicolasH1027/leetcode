407feeee14e9c20b579b969fa87b81b842ec3d3b
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-10 10:34:49 -0400

    .

--- 1522.DiameterofN-AryTree.py
+++ 1522.DiameterofN-AryTree.py
@@ -11,6 +11,8 @@ class Solution:
         """
         :type root: 'Node'
         :rtype: int
+        
+        O(NlogN), time, O(N) space
         """
         
         self.path = 0
@@ -28,6 +30,38 @@ class Solution:
                 self.path = max(self.path, res[-1] + res[-2])
             
             return res[-1] + 1                    
+        
+        dfs(root)
+        
+        return self.path
+
+class Solution:
+    def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        
+        O(NlogN), time, O(N) space
+        """
+        
+        self.path = 0
+        
+        def dfs(root):
+            if not root.children:
+                return 1
+            
+            first, second = 0, 0
+            
+            for child in root.children:
+                length = dfs(child)
+                if length > first:
+                    first, second = length, first
+                elif length > second:
+                    second = length
+                    
+            self.path = max(self.path, first + second)
+            
+            return max(first, second) + 1               
         
         dfs(root)
         
