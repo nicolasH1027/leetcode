33026b9902986f1faf1e3d101ef1ef3b34aaeb3a
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-14 20:03:36 -0400

    .

---
+++ 619.BiggestSingleNumber.sql
@@ -0,0 +1,28 @@
+-- # Write your MySQL query statement below
+SELECT
+    (
+    SELECT
+        num
+    FROM
+        my_numbers 
+    GROUP BY
+        num
+    HAVING
+        COUNT(*) = 1
+    ORDER BY
+        num DESC
+    LIMIT 1 
+    ) AS num;
+
+SELECT
+    MAX(num) AS num
+FROM
+    (SELECT
+        num
+    FROM
+        my_numbers
+    GROUP BY 
+        num
+    HAVING 
+        COUNT(num) = 1
+    ) AS t;
