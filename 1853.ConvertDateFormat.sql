9d59f732e8228e3d7445b0f7baaea4afeffd3463
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-19 09:02:24 -0400

    .

---
+++ 1853.ConvertDateFormat.sql
@@ -0,0 +1,12 @@
+SELECT
+    CONCAT(DAYNAME(day), ', ',MONTHNAME(day), ' ', DAY(day) ,', ',YEAR(day)) AS day
+FROM
+    Days
+
+
+-- ====================================================================
+
+SELECT
+    DATE_FORMAT(day, "%W, %M %e, %Y")
+FROM
+    Days
