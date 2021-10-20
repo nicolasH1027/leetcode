fc5eb386bc62f3b3678cd798cf6c24ba040f440a
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-20 10:33:16 -0400

    .

--- 569.MedianEmployeeSalary.sql
+++ 569.MedianEmployeeSalary.sql
@@ -4,14 +4,34 @@
 
 SELECT 
     Id, Company, Salary
-FROM (
+FROM 
+    (
     SELECT 
         *, 
         ROW_NUMBER() OVER(PARTITION BY COMPANY ORDER BY Salary ASC, Id ASC) AS RN_ASC,
         ROW_NUMBER() OVER(PARTITION BY COMPANY ORDER BY Salary DESC, Id DESC) AS RN_DESC
     FROM
-        Employee) AS temp
+        Employee
+    ) AS temp
 WHERE 
-    RN_ASC BETWEEN RN_DESC - 1 AND RN_DESC + 1
+    RN_ASC - RN_DESC BETWEEN - 1 AND 1
 ORDER BY 
     Company, Salary;
+
+
+-- ================================================================================================
+
+
+SELECT
+    Id, Company, Salary
+FROM
+    (
+    SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY Company ORDER BY Salary) AS ranking,
+        COUNT(Id) OVER(PARTITION BY Company) AS Total
+    FROM
+        Employee
+    ) AS T
+WHERE
+    ranking BETWEEN Total/2. AND Total/2. + 1
