6bf6a161e478bd4c07ab2059fcff9cd5411e9853
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-21 14:10:30 -0400

    .

---
+++ 253.MeetingRoomsII.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        
+        """
+        非常直觉的想法，生活中怎么安排？
+        假设第一组人开会，很自然开一间新会议室。
+        第二组来了，假如第一组没走，则需新开一间，如果走了
+        则使用原来那间（在这题中，我们只关心多少间会议室，而不是哪一间）。
+        一次类推，第三组来了，如果前面两组有结束的，那么不需要新开，否则新开一间。
+        
+        所以，每一次有新的团队要开会，只需要比较所有正在开会的团队的结束时间的最小值，
+        就可以确定是否需要新开一间会议室。
+        自然而然联想到需要使用 ‘优先队列’ 存储最小的结束时间。，
+        
+        O(nlogn) time complexity, O(n) space complexity
+        """
+        
+        if not intervals:
+            return 0
+        
+        intervals.sort(key = lambda x: x[0])
+        
+        heap = []
+        heapq.heappush(heap, intervals[0][1])
+        
+        for i in range(1, len(intervals)):
+            
+            if intervals[i][0] >= heap[0]:
+                heapq.heappop(heap)
+            
+            heapq.heappush(heap, intervals[i][1])
+        
+        return len(heap)
