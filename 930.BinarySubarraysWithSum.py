8f175407623957694a8c22f3622312e8ca4cd9bf
HuZiqing <nicolas.huziqing@gmail.com>
2021-10-22 14:29:18 -0400

    .

---
+++ 930.BinarySubarraysWithSum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+        """
+        :type nums: List[int]
+        :type goal: int
+        :rtype: int
+        """
+        """
+        same code with 1248
+        """
+        seen = {0:1}
+        cucnt = 0
+        cnt = 0
+        
+        for i in range(len(nums)):
+            cucnt += nums[i] % 2
+            
+            if cucnt - goal >= 0:
+                cnt += seen.get(cucnt - goal,0)
+            
+            seen[cucnt] = seen.get(cucnt, 0) + 1
+        
+        return cnt
+    
