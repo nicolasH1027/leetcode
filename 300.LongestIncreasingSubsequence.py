e45b40d2d9cd18a955036ea2d5aef73ab51f7d99
HuZiqing <nicolas.huziqing@gmail.com>
2021-11-27 09:48:43 -0500

    .

--- 300.LongestIncreasingSubsequence.py
+++ 300.LongestIncreasingSubsequence.py
@@ -9,3 +9,20 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
         
         return max(dp)
+    
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        top = []
+        
+        for num in nums:
+            
+            pos = bisect.bisect_left(top, num)
+            
+            if pos == len(top):
+                top.append(num)
+            else:
+                top[pos] = num
+        
+        return len(top)
+            
