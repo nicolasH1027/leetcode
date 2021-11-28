2d4cf01a27f82bb2873d049a2082d49ab846b130
HuZiqing <nicolas.huziqing@gmail.com>
2021-11-27 22:49:30 -0500

    .

--- 1143.LongestCommonSubsequence.py
+++ 1143.LongestCommonSubsequence.py
@@ -37,6 +37,21 @@ class Solution:
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+
+        m, n = len(text1), len(text2)
         
+        if m < n:
+            return self.longestCommonSubsequence(text2, text1)
         
-        0301Liuhaiming
+        dp = [0]*(n + 1)
+
+        for i in range(1, m + 1):
+            prev = 0
+            for j in range(1, n + 1):
+                tmp = dp[j]
+                if text1[i-1] == text2[j-1]:
+                    dp[j] = 1 + prev
+                else:
+                    dp[j] = max(dp[j], dp[j-1])
+                prev = tmp
+        return dp[n]
--- 494.TargetSum.py
+++ 494.TargetSum.py
@@ -1,3 +1,22 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
+        @lru_cache(maxsize=None)
+        def backtrack(numSum, i):
+            if i == len(nums):
+                if numSum == target:
+                    return 1
+                else:
+                    return 0
+            
+            result = 0
+            for op in [-1, 1]:
+                numSum += op*nums[i]
+                result += backtrack(numSum, i+1)
+                numSum -= op*nums[i]
+            return result 
+        
+        return backtrack(0, 0)
+    
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
