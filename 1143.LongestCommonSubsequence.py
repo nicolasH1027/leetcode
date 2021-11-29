b162b715ea8d23d762405f61fd84401e3af20e0d
HuZiqing <nicolas.huziqing@gmail.com>
2021-11-28 19:47:48 -0500

    .

--- 1143.LongestCommonSubsequence.py
+++ 1143.LongestCommonSubsequence.py
@@ -55,3 +55,18 @@ class Solution:
                     dp[j] = max(dp[j], dp[j-1])
                 prev = tmp
         return dp[n]
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        @lru_cache(maxsize = None)
+        def dp(i, j):
+            if i == len(text1) or j == len(text2):
+                return 0
+            
+            if text1[i] == text2[j]:
+                return 1 + dp(i+1, j+1)
+            else:
+                return max(dp[i+1][j], dp[i][j+1])
+        
+        return dp(0, 0)
