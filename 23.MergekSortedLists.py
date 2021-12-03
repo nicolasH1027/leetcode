4704f5e41a8403be932e6a89bb54b71cb009a8f7
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-03 15:12:59 -0500

    .

---
+++ 23.MergekSortedLists.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        if len(lists) == 0:
+            return None
+        
+        def dfs(lists, l, r):
+            if l == r:
+                return lists[l]
+            
+            mid = l + (r - l)//2
+            
+            left = dfs(lists, l, mid)
+            right = dfs(lists, mid+1, r)
+            
+            return self.mergelist(left, right)
+            
+        return dfs(lists, 0, len(lists) - 1)
+    
+    def mergelist(self, l1, l2):
+        
+        dummy = ListNode(0)
+        
+        pre = dummy
+        
+        while l1 and l2:
+            
+            if l1.val <= l2.val:
+                pre.next = l1
+                l1 = l1.next
+                
+            else:
+                pre.next = l2
+                l2 = l2.next
+                
+            pre = pre.next
+        
+        if l1:
+            pre.next = l1
+        else:
+            pre.next = l2
+        
+        return dummy.next
+        
+
+        # amount = len(lists)
+        # interval = 1
+        # while interval < amount:
+        #     for i in range(0, amount - interval, interval * 2):
+        #         lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+        #     interval *= 2
+        # return lists[0] if amount > 0 else None
