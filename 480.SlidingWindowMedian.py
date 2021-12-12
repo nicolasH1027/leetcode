7fd7ca1baab923a273b558a7c56a2d72307ce307
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-12 18:03:42 -0500

    .

--- 480.SlidingWindowMedian.py
+++ 480.SlidingWindowMedian.py
@@ -17,11 +17,41 @@ class Solution:
         return result
 
 
-# class Solution:
-#     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-#         "Two Heap"
 
-
-
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        "Two Heap"
+        
+        lo, hi, mediean = [], [], []
+        
+        dic = collections.defaultdict(int)
+        
+        for i in range(k):
+            heapq.heappush(lo, -nums[i])
+            
+        for i in range(k//2):
+            heapq.heappush(hi, -heapq.heappop(lo))
+        
+        mediean.append(-lo[0] if k % 2 else (hi[0] - lo[0])/2)
+            
+        for i in range(k, len(nums)):
+            
+            heapq.heappush(lo, -heapq.heappushpop(hi, nums[i]))
+            out_num = nums[i - k]
+            dic[out_num] += 1
+            if out_num > -lo[0]:
+                heapq.heappush(hi, -heapq.heappop(lo))
+            
+            while lo and dic[-lo[0]]:
+                dic[-lo[0]] -= 1
+                heapq.heappop(lo)
+            
+            while hi and dic[hi[0]]:
+                dic[hi[0]] -= 1
+                heapq.heappop(hi)
+            
+            mediean.append(-lo[0] if k % 2 else (hi[0] - lo[0])/2)
+        
+        return mediean
 
 
