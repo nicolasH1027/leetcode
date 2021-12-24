b32058b865ea70c6d8439550708a1422ae3ae423
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-24 17:38:56 -0500

    .

--- bit_operation.py
+++ bit_operation.py
@@ -15,6 +15,20 @@ or
 
 a & (a - 1) == 0 can be used test whether a is the exopnent of 2
 
+
+binary addition
+
+we use the & and << to get the carry
+
+add(a, b)
+
+while a:
+
+    carry = a & b
+    b = a ^ b
+    a = carry
+
+return b
 """
 
 
