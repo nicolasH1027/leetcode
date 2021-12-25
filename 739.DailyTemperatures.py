7a6ac5054abaf33a9725de8f2b9af625370a99ed
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-25 17:00:15 -0500

    .

--- 739.DailyTemperatures.py
+++ 739.DailyTemperatures.py
@@ -14,3 +14,27 @@ class Solution:
                 ans[top] = i - top
             stack.append(i)
         return ans
+    
+    
+    
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        
+        """
+        KMP algorithm
+        """
+
+        n = len(temperatures)
+        ans = [0]*n
+        
+        for i in range(n-2, -1, -1):
+            nx_day = i + 1
+            while temperatures[nx_day]  <= temperatures[i]:
+                if ans[nx_day]:
+                    nx_day += ans[nx_day]
+                else:
+                    break
+            else:
+                ans[i] = nx_day - i
+        
+        return ans
