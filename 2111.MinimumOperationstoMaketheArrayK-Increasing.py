549fc5751c3f5f357982ad155366636658e736f7
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-24 19:01:25 -0500

    .

---
+++ 2111.MinimumOperationstoMaketheArrayK-Increasing.py
@@ -0,0 +1,31 @@
+class Solution:
+    def kIncreasing(self, arr: List[int], k: int) -> int:
+        
+        """
+        k*(n/k*log(n/k)) = n*log(n/k)
+        """
+        
+        ans, pt, n = 0, 0, len(arr)
+        
+        for i in range(k):
+            
+            sub_len, sub_arr = 0, []
+            
+            pt = i
+            
+            while pt < n:
+                
+                sub_len += 1
+                
+                pos = bisect.bisect_right(sub_arr, arr[pt])
+                
+                if pos == len(sub_arr):
+                    sub_arr.append(arr[pt])
+                else:
+                    sub_arr[pos] = arr[pt]
+                
+                pt += k    
+            
+            ans += sub_len - len(sub_arr)
+
+        return ans
