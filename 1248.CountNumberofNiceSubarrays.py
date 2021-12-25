18ec6875b2aab67a32de8daf2312323c4c929296
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-25 17:53:35 -0500

    .

--- 1248.CountNumberofNiceSubarrays.py
+++ 1248.CountNumberofNiceSubarrays.py
@@ -9,17 +9,30 @@ class Solution(object):
         """
         compared with 560 subarray sum equals to k, they are the same with the technique prefix sum
         """
-        
-        seen = {0:1}
-        cucnt = 0
-        cnt = 0
+        n = len(nums)
+        seen = [0]*(n+1)
+        cucnt, cnt, seen[0] = 0, 0, 1
         
         for i in range(len(nums)):
             cucnt += nums[i] % 2
             
             if cucnt - k >= 0:
-                cnt += seen.get(cucnt - k)
+                cnt += seen[cucnt - k]
             
-            seen[cucnt] = seen.get(cucnt, 0) + 1
+            seen[cucnt] +=  1
         
         return cnt
+        
+        # seen = {0:1}
+        # cucnt = 0
+        # cnt = 0
+        
+        # for i in range(len(nums)):
+        #     cucnt += nums[i] % 2
+            
+        #     if cucnt - k >= 0:
+        #         cnt += seen.get(cucnt - k)
+            
+        #     seen[cucnt] = seen.get(cucnt, 0) + 1
+        
+        # return cnt
