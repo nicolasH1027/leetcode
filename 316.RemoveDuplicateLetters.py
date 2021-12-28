6efc3a9d71ed14c426c3d1949ddcecd80a98d51d
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-28 12:31:47 -0500

    .

---
+++ 316.RemoveDuplicateLetters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        stack, seen = [], set()
+        
+        last_occurence = {c:i for i, c in enumerate(s)}
+        
+        for i, c in enumerate(s):
+            
+            if c not in seen:
+                
+                while stack and c < stack[-1] and i < last_occurence[stack[-1]]:
+                    
+                    seen.remove(stack.pop())
+                    
+                    
+                stack.append(c)
+                seen.add(c)
+        
+        return ''.join(stack)
