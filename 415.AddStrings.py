54f12fe80a3697937c43385289379cdcb3ba22de
HuZiqing <nicolas.huziqing@gmail.com>
2021-12-29 09:22:31 -0500

    .

--- 415.AddStrings.py
+++ 415.AddStrings.py
@@ -1,25 +1,17 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        
         n = max(len(num1), len(num2))
-        num1 = num1.zfill(n)
-        num2 = num2.zfill(n)
+        num1, num2 = num1.zfill(n), num2.zfill(n)
         
-        carry = 0
-        result = []
+        ans, carry = collections.deque(), 0
+        
         for i in range(n-1, -1, -1):
-            tmp = ord(num1[i]) + ord(num2[i]) - 2*ord('0')
             
-            carry, val = divmod(tmp + carry, 10)
+            carry, digit = divmod(ord(num1[i]) + ord(num2[i]) - 2*ord('0') + carry, 10)
             
-            result.append(str(val))
-            
+            ans.appendleft(str(digit))
         
-        if carry == 1:
-            result.append('1')
-            
-        result.reverse()
+        if carry:
+            ans.appendleft('1')
         
-        return ''.join(result)
-            
-            
+        return ''.join(ans)
