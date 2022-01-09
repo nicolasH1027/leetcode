b048453892f0cff1dae08524475342286a3007ae
HuZiqing <nicolas.huziqing@gmail.com>
2022-01-09 14:34:22 -0500

    。

--- 828.CountUniqueCharactersofAllSubstringsofaGivenString.py
+++ 828.CountUniqueCharactersofAllSubstringsofaGivenString.py
@@ -22,3 +22,18 @@ class Solution:
             ans += cnt
         
         return ans
+
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        pos, ans, n = {c:[-1,-1] for c in s}, 0, len(s)
+        
+        for i, c in enumerate(s):
+            lag2, lag1 = pos[c]
+            ans += (lag1 - lag2) * (i - lag1)
+            pos[c] = [lag1, i]
+            
+        for c in pos.keys():
+            lag2, lag1 = pos[c]
+            ans += (n - lag1)*(lag1 - lag2)
+
+        return ans % (10**9 + 7)
