0bc91767af98101420934ad777f2f43ea82b219b
HuZiqing <nicolas.huziqing@gmail.com>
2022-01-09 13:31:01 -0500

    .

---
+++ 1151.MinimumSwapstoGroupAll1'sTogether.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        
+        n, one = len(n), sum(data)
+        MoveSum = sum(data[:one])
+        ans = MoveSum
+        
+        for i in range(one, n):
+            MoveSum += data[i]
+            MoveSum -= data[i-one]
+            ans = max(ans, MoveSum)
+        return one - ans
+            
+            
