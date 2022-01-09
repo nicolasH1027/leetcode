14f2eb1b63c50d45c87147d1f894f59bd6314b4b
HuZiqing <nicolas.huziqing@gmail.com>
2022-01-09 14:03:11 -0500

    .

--- 907.SumofSubarrayMinimums.py
+++ 907.SumofSubarrayMinimums.py
@@ -27,3 +27,30 @@ class Solution:
             ans += arr[i]*left[i]*right[i]
             
         return ans % (10**9 + 7)
+    
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        
+        """
+        我们需要一个单调递减栈（这里定义的递减是指stack从左往右递减）
+        这个栈用来维护原数组元素递减的index，换句话说，栈内元素对应的
+        原数组中的元素是递减的。这样我们就能找到left和right了
+        """
+        
+        ans = 0
+        "头尾两个哨兵变量可以保证首尾元素也能被考虑到"
+        nums = [0] + arr + [0]
+        stack = []
+        
+        for i, val in enumerate(arr):
+            
+            while stack and nums[stack[-1]] > val:
+                j = stack.pop()
+                k = stack[-1]
+                ans += nums[j]*(i - j)*(j - k)
+            
+            stack.append(i)
+        
+        return ans % (10**9 + 7)
+            
+        
