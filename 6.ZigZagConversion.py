2f61221a4d656635cc50028fd836761e4e866a1d
HuZiqing <nicolas.huziqing@gmail.com>
2022-01-19 20:21:18 -0500

    .

--- 6.ZigZagConversion.py
+++ 6.ZigZagConversion.py
@@ -81,4 +81,18 @@ class Solution:
             res[numRows-1] += s[i]
         
         return "".join(res)
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
         
+        ans = ['' for _ in range(numRows)]
+        i, flag = 0, -1
+        for val in s:
+            ans[i] += val
+            if i == 0 or i == numRows-1:
+                flag *= -1
+            i += flag
+            
+        return ''.join(ans)
