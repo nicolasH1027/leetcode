a8f0395836e5925d8f3cc43e48aec236384cb210
HuZiqing <nicolas.huziqing@gmail.com>
2022-01-21 11:32:26 -0500

    .

---
+++ 81.SearchinRotatedSortedArrayII.py
@@ -0,0 +1,36 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            
+            mid = left + (right - left)//2
+            
+            if nums[mid] == target:
+                return True
+            
+            if nums[mid] == nums[left]:
+                left += 1
+                
+            elif nums[mid] > nums[left]:
+                
+                if target >= nums[left] and target < nums[mid]:
+                    
+                    right = mid - 1
+                
+                else:
+                    
+                    left = mid + 1
+            
+            else:
+                
+                if target <= nums[right] and target > nums[mid]:
+                    
+                    left = mid + 1
+                    
+                else:
+                    
+                    right = mid - 1
+
+        return False
